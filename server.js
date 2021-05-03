c55ac3da77e0620c8f5aefbdf29edb60ead5fb54
James V <james.vanhoke@yahoo.com>
2021-05-03 13:48:36 -0700

    renamed db

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactcms");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/TournamentTracker");
 
 // Start the API server
 app.listen(PORT, function() {
