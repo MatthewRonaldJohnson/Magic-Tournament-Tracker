917224bc261b363b6a0e18ecfdae024ddf2350d2
James V <james.vanhoke@yahoo.com>
2021-05-04 13:49:48 -0700

    No Match Page

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 function NoMatch() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+<div class="d-flex justify-content-center align-items-center" style={{height:"80vh"}}>
+    <h2 className="row">404 Not Found</h2>
+</div>
+  );
 }
 
-export default NoMatch
+export default NoMatch;
