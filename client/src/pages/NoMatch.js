84cbd72c5093b2a38adaaba6ecea156d63636dfa
James V <james.vanhoke@yahoo.com>
2021-05-04 13:51:24 -0700

    linted

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 function NoMatch() {
   return (
-<div class="d-flex justify-content-center align-items-center" style={{height:"80vh"}}>
-    <h2 className="row">404 Not Found</h2>
-</div>
+    <div
+      class="d-flex justify-content-center align-items-center"
+      style={{ height: '80vh' }}
+    >
+      <h2 className="row">404 Not Found</h2>
+    </div>
   );
 }
 
