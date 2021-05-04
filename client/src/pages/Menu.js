4b76f830ee4bbd057429ec68c6c4dd773e282026
Matthew Johnson <MatthewRonaldJohnson@gmail.com>
2021-05-04 15:51:50 -0500

    created React Menu Page including Menu Button Component

--- client/src/pages/Menu.js
+++ client/src/pages/Menu.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import MenuButton from "../components/MenuButton";
 
 export default function Menu() {
     return (
-        <div>
-            
+        <div className="container my-3">
+
+            <div className="row">
+                <MenuButton location="/MatchInput" text="Match Input" btnClass="btn-primary"/>
+                <MenuButton location="/Analytics" text="Analytics" btnClass="btn-secondary"/>
+                <MenuButton location="/LifeCounter" text=" Life Counter" btnClass="btn-primary"/>
+                <MenuButton location="/CardSearch" text="Card Search" btnClass="btn-secondary"/>
+            </div>
         </div>
     )
 }
