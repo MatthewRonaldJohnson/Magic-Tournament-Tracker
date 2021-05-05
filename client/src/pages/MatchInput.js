88cc49a9440540cdd0bc35cfdb9ae3b800038410
James V <james.vanhoke@yahoo.com>
2021-05-04 20:15:20 -0700

    added context to Tournie card

--- client/src/pages/MatchInput.js
+++ client/src/pages/MatchInput.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import TournamentCard from "../components/TournamentCard"
+import React from 'react';
+import TournamentCard from '../components/TournamentCard';
+import { useStoreContext } from '../utils/GlobalState';
 
 export default function MatchInput() {
-    return (
-        <div>
-            <div class="container mt-2">
-            <TournamentCard />            
-            </div>
-        </div>
-    )
+  const state = useStoreContext();
+  return (
+    <div>
+      <div className="container mt-2">
+        <TournamentCard
+          deck={state[0].decks}
+          tournaments={state[0].tournaments}
+        />
+      </div>
+    </div>
+  );
 }
