d954fe9a4137772dd448b4b965f608f055ac86f4
James V <james.vanhoke@yahoo.com>
2021-05-04 19:44:17 -0700

    Tournament card + div wrapper

--- client/src/pages/MatchInput.js
+++ client/src/pages/MatchInput.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import TournamentCard from "../components/TournamentCard"
 
 export default function MatchInput() {
     return (
         <div>
-            
+            <div class="container mt-2">
+            <TournamentCard />            
+            </div>
         </div>
     )
 }
