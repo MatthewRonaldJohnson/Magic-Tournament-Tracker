417ba1c98dbdf269e5b5929e02aaf8aab209382a
Matthew Johnson <MatthewRonaldJohnson@gmail.com>
2021-05-06 14:54:18 -0500

    set up basic analytics page w/ dummy data

---
+++ client/src/components/SelectOption/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SelectOption(props) {
+    return (
+        <option value={props.id}>
+            {props.text}
+        </option>
+    )
+}
+
+export default SelectOption
