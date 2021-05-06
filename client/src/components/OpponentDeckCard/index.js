e9666e19bd0229581477a671cace55640fcd6d9c
James V <james.vanhoke@yahoo.com>
2021-05-06 14:53:44 -0700

    replaced html attr with react attr

--- client/src/components/OpponentDeckCard/index.js
+++ client/src/components/OpponentDeckCard/index.js
@@ -6,35 +6,35 @@ class OpponentDeckCard extends Component {
       <>
         <div id="opponent" className="deckInput col-12 col-lg-6 py-2">
           <div className="form-group">
-            <label for="userDeckname">Opponent Deck Name: </label>
-            <input id="userDeckName" className="form-control" type="text" />
+            <label htmlFor="oppDeckname">Opponent Deck Name: </label>
+            <input id="oppDeckName" className="form-control" type="text" />
             <div className="manaCheckBox white form-check form-check-inline">
               <input className="form-check-input" type="checkbox" id="oWhite" />
-              <label className="form-check-label" for="oWhite">
+              <label className="form-check-label" htmlFor="oWhite">
                 White
               </label>
             </div>
             <div className="manaCheckBox blue form-check form-check-inline">
               <input className="form-check-input" type="checkbox" id="oBlue" />
-              <label className="form-check-label" for="oBlue">
+              <label className="form-check-label" htmlFor="oBlue">
                 Blue
               </label>
             </div>
             <div className="manaCheckBox black form-check form-check-inline">
               <input className="form-check-input" type="checkbox" id="oBlack" />
-              <label className="form-check-label" for="oBlack">
+              <label className="form-check-label" htmlFor="oBlack">
                 Black
               </label>
             </div>
             <div className="manaCheckBox red form-check form-check-inline">
               <input className="form-check-input" type="checkbox" id="oRed" />
-              <label className="form-check-label" for="oRed">
+              <label className="form-check-label" htmlFor="oRed">
                 Red
               </label>
             </div>
             <div className="manaCheckBox green form-check form-check-inline">
               <input className="form-check-input" type="checkbox" id="oGreen" />
-              <label className="form-check-label" for="oGreen">
+              <label className="form-check-label" htmlFor="oGreen">
                 Green
               </label>
             </div>
