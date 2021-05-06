33e0231b35d02584cb1defad15c58ee6e666021f
James V <james.vanhoke@yahoo.com>
2021-05-06 15:02:56 -0700

    replaced html attr with react attr

--- client/src/components/MatchNotes/index.js
+++ client/src/components/MatchNotes/index.js
@@ -4,40 +4,40 @@ class MatchNotes extends Component {
   render() {
     return (
       <>
-        <div class="row my-1">
-          <div class="col-12">
-            <div class="form-row">
-              <div class="col-6">
+        <div className="row my-1">
+          <div className="col-12">
+            <div className="form-row">
+              <div className="col-6">
                 <label>Wins</label>
                 <input
                   type="number"
-                  class="form-control"
+                  className="form-control"
                   placeholder="Wins"
                   min="0"
                   max="2"
                 />
               </div>
-              <div class="col-6">
+              <div className="col-6">
                 <label>Losses</label>
                 <input
                   type="number"
-                  class="form-control"
+                  className="form-control"
                   placeholder="Losses"
                   min="0"
                   max="2"
                 />
               </div>
-              <div class="col-12">
+              <div className="col-12">
                 <label>Opponent Name</label>
                 <input
                   type="text"
-                  class="form-control"
+                  className="form-control"
                   placeholder="Opponent Name"
                 />
               </div>
-              <div class="col-12">
+              <div className="col-12">
                 <label>Notes</label>
-                <textarea class="form-control" name="" id=""></textarea>
+                <textarea className="form-control" name="" id=""></textarea>
               </div>
             </div>
           </div>
