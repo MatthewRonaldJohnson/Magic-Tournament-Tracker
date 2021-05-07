3f30ca871118c572d2de143348c5e866b290fb84
James V <james.vanhoke@yahoo.com>
2021-05-06 19:30:00 -0700

    match notes component + removing placeholder info

--- client/src/pages/MatchInput.js
+++ client/src/pages/MatchInput.js
@@ -3,6 +3,7 @@ import { useStoreContext } from '../utils/GlobalState';
 import TournamentCard from '../components/TournamentCard';
 import DeckCard from '../components/DeckCard';
 import OpponentDeckCard from '../components/OpponentDeckCard';
+import MatchNotes from '../components/MatchNotes';
 
 export default function MatchInput() {
   const [state, dispatch] = useStoreContext();
@@ -12,38 +13,11 @@ export default function MatchInput() {
         <TournamentCard tournaments={state.tournaments} />
         <div className="row">
           <DeckCard
-            decks={[
-              {
-                _id: 1,
-                deckName: 'MonoGreen',
-                whiteMana: false,
-                blueMana: false,
-                redMana: false,
-                greenMana: true,
-                blackMana: false,
-              },
-              {
-                _id: 2,
-                deckName: 'MonoRed',
-                whiteMana: false,
-                blueMana: false,
-                redMana: true,
-                greenMana: false,
-                blackMana: false,
-              },
-              {
-                _id: 3,
-                deckName: 'Rogues',
-                whiteMana: false,
-                blueMana: true,
-                redMana: false,
-                greenMana: false,
-                blackMana: true,
-              },
-            ]}
+            decks={state.decks}
           />
           <OpponentDeckCard />
         </div>
+        <MatchNotes />
       </div>
     </div>
   );
