f60f25f25f3a45d6f2754e12da4281e9bc3f2fd8
Matthew Johnson <MatthewRonaldJohnson@gmail.com>
2021-05-07 16:56:47 -0500

    made new build

---
+++ client/build/precache-manifest.fb5f22aa0654fd610789861da77908d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dfa9ed623d7cd4f2a8e924a24811902",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa441e4dc8ef7633ac63",
+    "url": "/static/css/2.9ee52aab.chunk.css"
+  },
+  {
+    "revision": "40216c937e38702ac4ba",
+    "url": "/static/css/main.5266a77e.chunk.css"
+  },
+  {
+    "revision": "aa441e4dc8ef7633ac63",
+    "url": "/static/js/2.b3aed1f1.chunk.js"
+  },
+  {
+    "revision": "1f52f68b7d4f2d2f5a21060e61b4b1da",
+    "url": "/static/js/2.b3aed1f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40216c937e38702ac4ba",
+    "url": "/static/js/main.220a31ef.chunk.js"
+  },
+  {
+    "revision": "2b8f580612459d80dc88",
+    "url": "/static/js/runtime-main.f65105ae.js"
+  },
+  {
+    "revision": "2a02b09603cec0049edc4930ae15a293",
+    "url": "/static/media/favicon.2a02b096.ico"
+  }
+]);
