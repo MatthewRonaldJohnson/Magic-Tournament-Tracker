5d35b7eb4a76e58c2c7781ac3c9fb0d12cfe321a
James V <james.vanhoke@yahoo.com>
2021-05-06 19:54:02 -0700

    saved fake data to a variable for testing

--- client/src/pages/MatchInput.js
+++ client/src/pages/MatchInput.js
@@ -7,13 +7,30 @@ import MatchNotes from '../components/MatchNotes';
 
 export default function MatchInput() {
   const [state, dispatch] = useStoreContext();
+  const decks= state.decks
+  const decksFD = [
+    {
+      _id: "6092fc902726131f443f9a1ea",
+      deckName: 'MonoGreen',
+      whiteMana: false,
+      blueMana: false,
+      redMana: false,
+      greenMana: true,
+      blackMana: false,
+    },
+  ]
+
+// Exact same structure, but if you pass in the FakeData (decksFD) on line 33 instead of decks it works.
+  console.log("Real Data", decks)
+  console.log("Fake Data", decksFD)
+
   return (
     <div>
       <div className="container mt-2">
         <TournamentCard tournaments={state.tournaments} />
         <div className="row">
           <DeckCard
-            decks={state.decks}
+            decks={decks}
           />
           <OpponentDeckCard />
         </div>
