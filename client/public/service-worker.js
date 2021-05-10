137c2af13a8c7bcc967f6e21e2bae4354347d915
Matthew Johnson <MatthewRonaldJohnson@gmail.com>
2021-05-10 10:31:38 -0500

    converted to PWA (no offline function yet)

---
+++ client/public/service-worker.js
@@ -0,0 +1,25 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html'
+];
+
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches
+            .open(PRECACHE)
+            .then((cache) => cache.addAll(FILES_TO_CACHE))
+            .then(self.skipWaiting())
+    );
+});
+
+self.addEventListener('fetch', function(event){
+    event.respondWith(
+        caches.match(e.request).then(function(response){
+            return response || fetch(event.request);
+        })
+    )
+})
