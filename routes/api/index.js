b8de0d4196071e6602c931462f1b7237bbb14171
James V <james.vanhoke@yahoo.com>
2021-05-09 18:08:39 -0700

    removed vestigial code

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,8 @@
 const router = require("express").Router();
-const postRoutes = require("./posts");
 const userRoutes = require("./user");
 const inputRoutes = require('./input')
 
 // Post routes
-router.use("/posts", postRoutes);
 router.use("/user", userRoutes);
 router.use("/input", inputRoutes)
 
